512644842b541e9eee64873c2bfd2c99eebc7b0e
Paulo Henrique <tui-lol@hotmail.com>
2023-03-14 11:51:58 -0300

    Getting a user by ID

--- src/main/java/com/paulohenrique/workshopmongo/service/UserService.java
+++ src/main/java/com/paulohenrique/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.paulohenrique.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paulohenrique.workshopmongo.domain.User;
 import com.paulohenrique.workshopmongo.repository.UserRepository;
+import com.paulohenrique.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -14,7 +16,15 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
+	
 	public List<User>findAll(){
 		return repo.findAll();
 	}
-}
+	public User findById(String id) {
+		Optional<User>obj = repo.findById(id);
+		return obj.orElseThrow(() ->new ObjectNotFoundException("Objeto não encontrado"));
+		}
+		
+	}
+	
+
