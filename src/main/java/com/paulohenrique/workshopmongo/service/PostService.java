48496276cbd0d7186fc7d872278608b53c11fbac
Paulo Henrique <tui-lol@hotmail.com>
2023-03-14 23:13:34 -0300

    getting a post by ID

---
+++ src/main/java/com/paulohenrique/workshopmongo/service/PostService.java
@@ -0,0 +1,31 @@
+package com.paulohenrique.workshopmongo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulohenrique.workshopmongo.domain.Post;
+import com.paulohenrique.workshopmongo.domain.User;
+import com.paulohenrique.workshopmongo.repository.PostRepository;
+import com.paulohenrique.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+
+	@Autowired
+	private PostRepository repo;
+	
+	//public List<User>findAll(){
+	//	return repo.findById();
+	//}
+	public Post findById(String id) {
+		Optional<Post>obj = repo.findById(id);
+		return obj.orElseThrow(() ->new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	}
+	
+
